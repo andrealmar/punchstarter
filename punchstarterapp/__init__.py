f96dabd1ca794f7a67d9ec383fb110d60ab110cf
Andre Almar <andre@y7mail.com>
2016-01-20 13:02:57 -0200

    __init__.py modified to deal with database stuff

--- punchstarterapp/__init__.py
+++ punchstarterapp/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 
 app = Flask(__name__)
+app.config.from_object('punchstarterapp.default_settings')
 manager = Manager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+from punchstarterapp.models import *
+
 @app.route("/")
 def hello():
     return render_template("index.html")
