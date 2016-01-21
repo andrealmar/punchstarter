655408acb6b92efa2f65c7a2ecc583339102cc4c
Andre Almar <andre@y7mail.com>
2016-01-21 12:19:37 -0200

    new route project detail created

--- punchstarterapp/__init__.py
+++ punchstarterapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
@@ -43,3 +43,11 @@ def create():
         db.session.commit()
 
         return redirect(url_for('create'))
+
+@app.route("/projects/<int:project_id>")
+def project_detail(project_id):
+    project = db.session.query(Project).get(project_id)
+    if project is None:
+        abort(404)
+
+    return render_template( "project_detail.html", project = project)
