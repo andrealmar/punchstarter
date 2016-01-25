607e9ebb49d32730bdb7cd06b7efd02bd8c0de99
Andre Almar <andre@y7mail.com>
2016-01-25 17:19:33 -0200

    replace the placeholder with image_path property

--- punchstarterapp/default_settings.py
+++ punchstarterapp/default_settings.py
@@ -1,11 +1,12 @@
 import os
+import cloudinary
 
 DEBUG=True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/app.db"
 
 cloudinary.config(
-  cloud_name = "sample",
-  api_key = "874837483274837",
-  api_secret = "a676b67565c6767a6767d6767f676fe1"
+  cloud_name = "andrealmar",
+  api_key = "668886311822753",
+  api_secret = "69dYS6OFx-hDtNZmZ6YLW1P3tX8"
 )
