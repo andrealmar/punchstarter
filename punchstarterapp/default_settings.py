33fac2843686596798c24be87ceaa7e4766f2fe4
Andre Almar <andre@y7mail.com>
2016-01-25 16:54:09 -0200

    added cloudinary integration

--- punchstarterapp/default_settings.py
+++ punchstarterapp/default_settings.py
@@ -3,3 +3,9 @@ import os
 DEBUG=True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/app.db"
+
+cloudinary.config(
+  cloud_name = "sample",
+  api_key = "874837483274837",
+  api_secret = "a676b67565c6767a6767d6767f676fe1"
+)
