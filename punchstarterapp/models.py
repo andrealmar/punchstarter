bd29b9646d694961fa49b1ba4a206da2231af294
Andre Almar <andre@y7mail.com>
2016-02-02 22:50:55 -0200

    pep8 adjustments

--- punchstarterapp/models.py
+++ punchstarterapp/models.py
@@ -3,17 +3,25 @@ from sqlalchemy.sql import func
 import datetime
 import cloudinary.utils
 
+
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
-    project = db.relationship('Project', backref= 'creator')
-    pledges = db.relationship('Pledge', backref='pledger', foreign_keys='Pledge.member_id')
+    project = db.relationship('Project', backref='creator')
+    pledges = db.relationship(
+        'Pledge', backref='pledger',
+        foreign_keys='Pledge.member_id'
+        )
 
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
+    member_id = db.Column(
+        db.Integer,
+        db.ForeignKey('member.id'),
+        nullable=False
+        )
     name = db.Column(db.String(100))
     short_description = db.Column(db.Text)
     long_description = db.Column(db.Text)
@@ -22,7 +30,11 @@ class Project(db.Model):
     time_starts = db.Column(db.DateTime)
     time_end = db.Column(db.DateTime)
     time_created = db.Column(db.DateTime)
-    pledges = db.relationship('Pledge', backref='project', foreign_keys='Pledge.project_id')
+    pledges = db.relationship(
+        'Pledge',
+        backref='project',
+        foreign_keys='Pledge.project_id'
+        )
 
     @property
     def num_pledges(self):
@@ -51,9 +63,18 @@ class Project(db.Model):
     def image_path(self):
         return cloudinary.utils.cloudinary_url(self.image_filename)[0]
 
+
 class Pledge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    member_id = db.Column(
+        db.Integer,
+        db.ForeignKey('member.id'),
+        nullable=False
+        )
+    project_id = db.Column(
+        db.Integer,
+        db.ForeignKey('project.id'),
+        nullable=False
+        )
     amount = db.Column(db.Integer)
     time_created = db.Column(db.DateTime)
